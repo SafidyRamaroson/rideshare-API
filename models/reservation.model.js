ab81ae3d6a1e66d09e3d1c31af08191d60bff888
SafidyRM <safidyramaroson.patrick@gmail.com>
2024-03-21 11:23:33 +0300

    Reservation model created

---
+++ models/reservation.model.js
@@ -0,0 +1,31 @@
+module.exports = (database,DataTypes)=>{
+    const Reservation = sequelize.define('Reservation', {
+        ReservationID: {
+          type: DataTypes.INTEGER,
+          primaryKey: true,
+          autoIncrement: true
+        },
+        reservationDate: {
+          type: DataTypes.DATE
+        },
+        reservationStatus: {
+          type: DataTypes.STRING
+        },
+        amountPaid: {
+          type: DataTypes.FLOAT
+        },
+        createdAt: {
+          type: DataTypes.DATE,
+          defaultValue: DataTypes.NOW
+        },
+        updatedAt: {
+          type: DataTypes.DATE,
+          defaultValue: DataTypes.NOW
+        }
+      });
+      
+    Reservation.belongsTo(require("./user.model")(database,DataTypes),{foreignKey:'PassengerID'});
+    Reservation.belongsTo(require("./trip.model")(database,DataTypes),{foreignKey:'TripID'});
+
+    return Reservation;
+}
