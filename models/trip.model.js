f25bd90f2860825689f7510c568f503f3da3d4f4
SafidyRM <safidyramaroson.patrick@gmail.com>
2024-03-21 11:11:09 +0300

    Trip model created

---
+++ models/trip.model.js
@@ -0,0 +1,40 @@
+module.exports = (database,DataTypes) => {
+    const Trip = database.define('Trip', {
+        TripID: {
+          type: DataTypes.INTEGER,
+          primaryKey: true,
+          autoIncrement: true
+        },
+        departureLocation: {
+          type: DataTypes.STRING
+        },
+        destination: {
+          type: DataTypes.STRING
+        },
+        departureTime: {
+          type: DataTypes.DATE
+        },
+        pricePerPassenger: {
+          type: DataTypes.FLOAT
+        },
+        availableSeats: {
+          type: DataTypes.INTEGER
+        },
+        tripDescription: {
+          type: DataTypes.STRING
+        },
+        createdAt: {
+          type: DataTypes.DATE,
+          defaultValue: DataTypes.NOW
+        },
+        updatedAt: {
+          type: DataTypes.DATE,
+          defaultValue: DataTypes.NOW
+        }
+      });
+
+    Trip.belongsTo(require("./user.model")(database,DataTypes), { foreignKey: 'DriverID' });
+    Trip.belongsTo(require("./drivingLicense.model"), { foreignKey: 'VehicleID' });
+
+    return Trip ;
+}
