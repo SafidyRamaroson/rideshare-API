71f6bd072db27772c5d818d25c5f150710eca2f7
SafidyRM <safidyramaroson.patrick@gmail.com>
2024-03-23 13:14:23 +0300

    Implemented User Registration and Login Data Validation

---
+++ utils/user/parseData.js
@@ -0,0 +1,25 @@
+const { z }=  require("zod");
+const is = require("is-zod");
+
+const parseRegisterData = (data)=>{
+
+    const schema = z.object({
+        firstName:z.string().trim().max(20),
+        lastName:z.string().trim().max(50),
+        email:z.string().trim().email(),
+        password:z.string().trim().min(8).max(20),
+    })
+
+    return schema.safeParse(data);
+}
+
+const parseLoginData = (data)=>{
+    const schema = z.object({
+        email:z.string().trim().email(),
+        password:z.string().trim().min(8).max(20),
+    })
+    return schema.safeParse(data);
+}
+
+
+module.exports = { parseRegisterData,parseLoginData };
