7eeef8e32753cc4396056d07df3ba58ae220fe04
SafidyRM <safidyramaroson.patrick@gmail.com>
2024-05-30 12:31:48 +0300

    chore server.js

--- server.js
+++ server.js
@@ -1,10 +1,6 @@
 const http = require("http");
 const app = require("./app");
 const { PORT } = require("./config/config");
-const db = require("./models/index.js");
-const { faker } = require('@faker-js/faker');
-const availableProvince = require("./const/availableProvince.js");
-const generateRandomUser = require("./lib/faker/generateRandomUser.js");
 require("dotenv").config();
 
 
@@ -16,9 +12,6 @@ server.listen(PORT, ()=>{
     console.log(`Server is running on  http://localhost:${PORT}`);
 });
 
-// db.databaseConf.sync();
-generateRandomUser(100);
-// db.dropRideShareTable();
 
 
 
