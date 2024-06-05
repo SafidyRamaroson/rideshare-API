c5dbe96aadf16e598e8281c7b8e3bd820259985d
SafidyRM <safidyramaroson.patrick@gmail.com>
2024-06-05 13:58:17 +0300

    fix( getPagination ) :initailize  offset to 1

--- utils/getPagination.js
+++ utils/getPagination.js
@@ -2,7 +2,7 @@ const getPagination = ( page,size ) => {
     // considerons que size egale  à 6
 
     const limit  = size ? +size : 4
-    const offset = page>=1 ? limit * (page-1): 0;
+    const offset = page>=1 ? limit * (page-1): 1;
     return { limit , offset };
 }
 
