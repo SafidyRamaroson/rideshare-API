554825a7b66db2231edad9d04f7c2adc007549df
SafidyRM <safidyramaroson.patrick@gmail.com>
2024-06-05 13:11:35 +0300

    chore(getPagination.js):update get pagination

--- utils/getPagination.js
+++ utils/getPagination.js
@@ -1,12 +1,8 @@
 const getPagination = ( page,size ) => {
     // considerons que size egale  à 6
+
     const limit  = size ? +size : 4
     const offset = page>=1 ? limit * (page-1): 0;
-    // const offset = 2
-    console.log("page")
-    console.log(page)
-    console.log("limit")
-    console.log(limit,offset)
     return { limit , offset };
 }
 
