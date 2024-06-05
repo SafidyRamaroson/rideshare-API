3eee5fabe6e62c129fc7bbe50d9063fc312b2b36
SafidyRM <safidyramaroson.patrick@gmail.com>
2024-06-05 13:37:49 +0300

    chore(getTripsAvailableSearch.service.js):update get Trips Search  Service

--- services/trips/getTripsAvailableSearch.js
+++ services/trips/getTripsAvailableSearch.js
@@ -1,8 +1,8 @@
-const { Op, Sequelize } = require("sequelize");
+const { Op } = require("sequelize");
 const db = require("../../models");
 const getPagination = require("./../../utils/getPagination");
 const getPagingData = require("./../../utils/getPagingData");
-const getFormattedTripWithStopData = require("./utils/getFormattedTripWithStopData");
+const getFormattedTripsListWithStops = require("./formattedTripsListWithStops");
 
 
 const getTripsAvailableSearch = async(page,size,req)=> {
@@ -16,59 +16,44 @@ const getTripsAvailableSearch = async(page,size,req)=> {
     } = req.body;
 
     const { limit , offset } = getPagination(page, size);
-
+    
     const whereConditions ={
         [Op.and]: [
           {
             [Op.or]: [
               { departurePrecise: { [Op.like]: `%${departurePrecise}%` } },
               { destinationPrecise: { [Op.like]:`%${destinationPrecise}%` } },
-              { departureDate: dateOfDeparture?.split("T")[0]},
-              { returnDate: returnOfDate?.split("T")[0]}
+              { departureDate: dateOfDeparture?.split("T")[0] ?? ""},
+              { returnDate: returnOfDate?.split("T")[0] ?? ""}
             ]
           },
           { seats: { [Op.gte]: seats } }
         ]
       }
-      
-      const data = await db.trip.findAndCountAll({
-        offset: offset,
-        limit: limit,
-        attributes: {
-          exclude: ["updatedAt"]
-        },
-        include: [{
-            model: db.stop,
-            required: false
-        }],
-        where: whereConditions,
-      });
 
-    const trips = await db.trip.findAll({
-        attributes: {
+    const trips = await db.trip.findAndCountAll({
+      limit,
+      offset,
+      attributes: {
             exclude: ["updatedAt"]
-        }, 
-        include: [{
-            model: db.stop,
-            required: false
-        }],
+        },
         where: whereConditions,
     });
 
-    const totalItems = trips.length;
-    
-    // Chercher le nombre de places disponibles pour chaque voyage (si nécessaire)
-    const {tripsListData:tripsIncludeStopsListData } = getPagingData(data,page,limit);
- 
-    const formattedTripsListWithStops = []
-    for(const tripIncldudeStopData of tripsIncludeStopsListData){
-        formattedTripsListWithStops.push(await getFormattedTripWithStopData(tripIncldudeStopData))
-    }
+    const countTripsAvailble = await db.trip.count({
+       attributes: {
+            exclude: ["updatedAt"]
+        },
+        where: whereConditions,
+    }) 
 
+    // Chercher le nombre de places disponibles pour chaque voyage (si nécessaire)
+    const { tripsListData } = getPagingData(trips,page,limit)
+    const formattedTripsListWithStops = await  getFormattedTripsListWithStops(tripsListData)
     return {
         trips:formattedTripsListWithStops,
-        totalPages:(Math.ceil(totalItems/size)== 0 && totalItems == 0) ? 0:Math.ceil(totalItems/size),
-        totalItems:totalItems,
+        totalPages:(Math.ceil(countTripsAvailble/size)== 0 && countTripsAvailble == 0) ? 0:Math.ceil(countTripsAvailble/size),
+        totalItems:countTripsAvailble,
     }
 }
 
